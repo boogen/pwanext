82ad347be664c42e7353ae014ccc46a0b6165550
Marcin Bugala <marcin.bugala@tensquaregames.com>
2024-02-04 17:11:26 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import PWAInstallComponent from "./components/pwa-install";
 
 export default function Home() {
-  
+
   return (
     <>
-      <PWAInstallComponent onInstallSuccess={undefined} onInstallFail={undefined} onUserChoiceResult={undefined} onInstallAvailable={undefined} onInstallHowTo={undefined} onInstallGallery={undefined} />
+      <PWAInstallComponent onInstallSuccess={undefined} onInstallFail={(msg: any) => {alert(msg)}} onUserChoiceResult={undefined} onInstallAvailable={undefined} onInstallHowTo={undefined} onInstallGallery={undefined} />
     </>
   );
 }
